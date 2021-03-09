d354d315f1763eefe29554eba01eae16a06809e5
mike <mrossi09@gmail.com>
2021-03-09 13:50:42 -0500

    added utils, codebook scrapper

--- meps_dev/components/management/commands/scrap_meps_codebook.py
+++ meps_dev/components/management/commands/scrap_meps_codebook.py
@@ -1,29 +1,29 @@
 from django.core.management.base import BaseCommand
-from meps_dev.components.scrapper import ScrapMEPS
+from meps_dev.components.scrapper import ScrapMEPSCodebook
 
 
 class Command(BaseCommand):
     """ Management command to scrap data files from the MEPS site. Run in terminal.
         ex:
-            python manage.py scrap_meps
-            python manage.py scrap_meps --years 2016 2019 --download_dir "custom/path" --sleep 25
+            python manage.py scrap_meps_codebook
+            python manage.py scrap_meps_codebook --years 2016 2019 --output_dir "custom/path" --sleep 25
     """
 
-    help = "Scraps the MEPS page for zipped data files "
+    help = "Scraps the MEPS page variable information "
 
     def add_arguments(self, parser):
         parser.add_argument("--years", nargs="*", type=int, help="declare a years to scrap data for: 2019 2020")
 
-        parser.add_argument("--download_dir", nargs="?", help="declare the path to the download directory")
+        parser.add_argument("--output_dir", nargs="?", help="declare the path to the download directory")
 
         parser.add_argument("--sleep", nargs="?", type=int, help="declare how many seconds to wait between downloads")
 
     def handle(self, *args, **options):
 
         kwargs = {}
-        for arg in ["years", "download_dir", "sleep"]:
+        for arg in ["years", "output_dir", "sleep"]:
             if options[arg] is not None:
                 kwargs.update({arg: options[arg]})
 
-        scrapper = ScrapMEPS(**kwargs)
+        scrapper = ScrapMEPSCodebook(**kwargs)
         scrapper.run()
